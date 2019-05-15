a4d337b781abeca0f05aa798289b69999736f4a3
Haydar Ali Ismail <haydar_ai@yahoo.com>
2019-05-15 11:57:05 +0200

    Set email as primary key for user

--- api/models/user_model.py
+++ api/models/user_model.py
@@ -3,7 +3,7 @@ import mongoengine_goodjson as gj
 
 
 class User(gj.Document):
-    email = db.StringField(required=True)
+    email = db.StringField(required=True, primary_key=True)
     name = db.StringField(required=True, max_length=100)
     password = db.StringField(required=True)
     meta = {'collection': 'users'}
